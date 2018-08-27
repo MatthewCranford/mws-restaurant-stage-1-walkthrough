dc7f3b1641e0f710ad8b50f004728cdf88f31a50
Matthew Cranford <mcranfordmail@gmail.com>
2018-08-27 12:08:50 -0600

    Add service worker listeners

--- sw.js
+++ sw.js
@@ -1,1 +1,9 @@
 console.log('Service Worker: Registered');
+
+self.addEventListener('install', function(e) {
+    console.log(e);
+});
+
+self.addEventListener('fetch', function(e) {
+    console.log(e.request);
+});
